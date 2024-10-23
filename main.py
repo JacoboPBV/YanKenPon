361c88856b97e7d9c20d8ddd2189cc5f8d0cd6ff
Jacobo Pita <jpitabaamondev23@fpcoruna.afundacion.org>
2024-10-23 11:56:18 +0200

    Comentarios añadidos

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from math import floor
 from random import random
 from tkinter import messagebox
 
+# Ventana que va a contener el menú
 main = tk.Tk()
 main.title("Piedra, papel, tijera")
 main.geometry("200x100+50+50")
 
 
 def nueva_partida_vs_ia():
+    # Ventana para la partida contra la máquina
     root = tk.Toplevel()
     root.title("Jugador vs IA")
     root.geometry("800x600+300+50")
@@ -29,7 +31,7 @@ def nueva_partida_vs_ia():
         añadir_puntos(elegir_ganador())
 
     def jugar_IA():
-        jugada_IA = floor((random() * 100) % 3)
+        jugada_IA = floor((random() * 100) % 3)  # Selecciona un número del 0 al 2 aleatorio
         if jugada_IA == 0:
             value_jugada_IA.set("Piedra")
         elif jugada_IA == 1:
@@ -37,13 +39,13 @@ def nueva_partida_vs_ia():
         elif jugada_IA == 2:
             value_jugada_IA.set("Tijeras")
         else:
-            root.attributes("-disabled", True)
-            messagebox.showinfo("Número no encontrado", "Cerrando la aplicación...")
+            root.attributes("-disabled", True)  # Deshabilita la ventana de juego para impedir bugs
+            messagebox.showinfo("Número no encontrado", "Cerrando la partida...")
             root.destroy()
 
     def elegir_ganador():
-        resultado = None
-        root.attributes("-disabled", True)
+        resultado = None  # Inicializamos la variable para guardar al ganador
+        root.attributes("-disabled", True)  # Deshabilita la ventana de juego para impedir bugs
         if value_jugada_player.get() == "Piedra":
             if value_jugada_IA.get() == "Piedra":
                 resultado = 0
@@ -87,19 +89,20 @@ def nueva_partida_vs_ia():
             messagebox.showinfo("Error inesperado", "Cerrando la aplicación...")
             root.quit()
 
-        root.attributes("-disabled", False)
+        root.attributes("-disabled",
+                        False)  # Habilita de nuevo la ventana después de cerrar el messagebox correspondiente
         return resultado
 
     def añadir_puntos(res):
-        if res > 0:
+        if res > 0:  # Si gana el jugador, actualiza su marcador
             puntos_player.set(puntos_player.get() + 1)
             puntuacion_jugador1.config(text="PUNTUACIÓN JUGADOR: " + str(puntos_player.get()))
-        elif res < 0:
+        elif res < 0:  # Si gana la máquina, actualiza su marcador
             puntos_IA.set(puntos_IA.get() + 1)
             puntuacion_jugador2.config(text="PUNTUACIÓN IA: " + str(puntos_IA.get()))
 
-        if puntos_IA.get() == 3 or puntos_player.get() == 3:
-            root.attributes("-disabled", True)
+        if puntos_IA.get() == 3 or puntos_player.get() == 3:  # Si la partida se acaba, elige ganador
+            root.attributes("-disabled", True)  # Deshabilita la ventana de juego para impedir bugs
             if puntos_IA.get() == 3:
                 messagebox.showinfo("Partida terminada", "GANADOR: IA")
             else:
@@ -107,17 +110,21 @@ def nueva_partida_vs_ia():
 
             root.destroy()  # Cierra la partida, pero no la aplicación
 
-    tk.Label(root).grid(row=0, column=0)
+    tk.Label(root).grid(row=0, column=0)  # Label decorativa para centrar el grid
 
+    # Jugadas
     value_jugada_player = tk.StringVar()
     value_jugada_player.set(str(None))
     value_jugada_IA = tk.StringVar()
     value_jugada_IA.set(str(None))
+
+    # Puntuación
     puntos_player = tk.IntVar()
     puntos_player.set(0)
     puntos_IA = tk.IntVar()
     puntos_IA.set(0)
 
+    # Grid
     puntuacion_jugador1 = tk.Label(root, text="PUNTUACIÓN JUGADOR: " + str(puntos_player.get()))
     puntuacion_jugador1.grid(row=1, column=1, columnspan=2, pady=20)
     puntuacion_jugador2 = tk.Label(root, text="PUNTUACIÓN IA: " + str(puntos_IA.get()))
@@ -129,8 +136,9 @@ def nueva_partida_vs_ia():
     tijeras = tk.Button(root, text="TIJERAS", command=jugar_tijeras)
     tijeras.grid(row=2, column=5, padx=15)
 
-    tk.Label(root).grid(row=10, column=10)
+    tk.Label(root).grid(row=10, column=10)  # Label decorativa para centrar el grid
 
+    # Configuración grid
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(10, weight=1)
@@ -138,6 +146,7 @@ def nueva_partida_vs_ia():
 
 
 def nueva_partida_vs_jugador():
+    # Ventana para la partida contra otro jugador
     root = tk.Toplevel()
     root.title("Jugador vs Jugador")
     root.geometry("800x600+300+50")
@@ -173,8 +182,8 @@ def nueva_partida_vs_jugador():
             añadir_puntos(elegir_ganador())
 
     def elegir_ganador():
-        resultado = None
-        root.attributes("-disabled", True)
+        resultado = None  # Inicializamos la variable para guardar al ganador
+        root.attributes("-disabled", True)  # Deshabilita la ventana de juego para impedir bugs
         if value_jugada_player[0] == "Piedra":
             if value_jugada_player[1] == "Piedra":
                 resultado = 0
@@ -227,36 +236,43 @@ def nueva_partida_vs_jugador():
             messagebox.showinfo("Error inesperado", "Cerrando la aplicación...")
             root.quit()
 
-        root.attributes("-disabled", False)
+        root.attributes("-disabled",
+                        False)  # Habilita de nuevo la ventana después de cerrar el messagebox correspondiente
         return resultado
 
     def añadir_puntos(res):
-        if res > 0:
+        if res > 0:  # Si gana el jugador 1
             puntos_player1.set(puntos_player1.get() + 1)
             puntuacion_jugador1.config(text="PUNTUACIÓN JUGADOR 1: " + str(puntos_player1.get()))
-        elif res < 0:
+        elif res < 0:  # Si gana el jugador 2
             puntos_player2.set(puntos_player2.get() + 1)
             puntuacion_jugador2.config(text="PUNTUACIÓN JUGADOR 2: " + str(puntos_player2.get()))
 
-        if puntos_player2.get() == 3 or puntos_player1.get() == 3:
+        if puntos_player2.get() == 3 or puntos_player1.get() == 3:  # Si la partida se acaba, elige ganador
             if puntos_player2.get() == 3:
-                root.attributes("-disabled", True)
+                root.attributes("-disabled", True)  # Deshabilita la ventana del juego para impedir bugs
                 messagebox.showinfo("Partida terminada", "GANADOR: Jugador 2")
             else:
                 messagebox.showinfo("Partida terminada", "GANADOR: Jugador 1")
 
             root.destroy()  # Cierra la partida, pero no la aplicación
 
-    tk.Label(root).grid(row=0, column=0)
+    tk.Label(root).grid(row=0, column=0)  # Label decorativa para centrar el grid
 
+    # Jugadas
     value_jugada_player = ["", ""]
+
+    # Turno
     turno = tk.IntVar()
     turno.set(1)
+
+    # Puntuación
     puntos_player1 = tk.IntVar()
     puntos_player1.set(0)
     puntos_player2 = tk.IntVar()
     puntos_player2.set(0)
 
+    # Grid
     marcador_turno = tk.Label(root, text="TURNO: JUGADOR " + str(turno.get()))
     marcador_turno.grid(row=1, column=2, columnspan=3)
     puntuacion_jugador1 = tk.Label(root, text="PUNTUACIÓN JUGADOR 1: " + str(puntos_player1.get()))
@@ -270,16 +286,20 @@ def nueva_partida_vs_jugador():
     tijeras = tk.Button(root, text="TIJERAS", command=jugar_tijeras)
     tijeras.grid(row=3, column=5, padx=15)
 
-    tk.Label(root).grid(row=10, column=10)
+    tk.Label(root).grid(row=10, column=10)  # Label decorativa para centrar el grid
 
+    # Configuración grid
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(10, weight=1)
     root.grid_columnconfigure(10, weight=1)
 
+
 def salir():
     main.destroy()
 
+
+# Menu
 menu = tk.Menu(main)
 main.config(menu=menu)
 
